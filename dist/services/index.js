c5a70dfdfe5edd35786a55ed35ecbe81e0b101f7
asd <asd>
2020-06-20 21:14:36 -0500

    removin dist

--- dist/services/index.js
+++
@@ -1,5 +0,0 @@
-var data = require('./data/index').data;
-var ipc = require('./ipc/index').ipc;
-var SERVICES = { data: data, ipc: ipc };
-module.exports = { SERVICES: SERVICES };
-//# sourceMappingURL=index.js.map
--- dist/services/ipc/index.js
+++
@@ -1,4 +0,0 @@
-var bind = require('./bind').bind;
-var ipc = { bind: bind };
-module.exports = { ipc: ipc };
-//# sourceMappingURL=index.js.map
